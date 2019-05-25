de08ea2d0807998d32058300d95047934874eac9
AlexBobryshev <darknessrider1@rambler.ru>
2019-05-25 21:16:06 +0300

    6th commit (with alert)

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ var count = 0;
 function showDate() {
     var date = new Date();
     document.getElementById("date").innerHTML = "last time: " + date;
+    if (count % 10 == 0) alert ("Congs! Buttons were pushed 10 times")
 } 
